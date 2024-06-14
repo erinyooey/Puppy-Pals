891bb59be4b4725e08605eed2baac93c52f3a46c
Erin Yoo <yooeh@masters.edu>
2024-06-13 19:39:47 -0700

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,33 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   console.log("puppyList: ", puppyList)
 
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log("Feature pup: ", featuredPup
+
+  )
+
   return (
     <div className='App'>
       {
         puppies.map((puppy)=>{
-          return <p key={puppy.id}>{puppy.name}</p>
+          // return (<p key={puppy.id}>{puppy.name}</p>)
+          return(<p onClick={()=> {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>)
         })
       }
+
+      {featPupId && (<div>
+        <h2 className='pupName'>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+        
+      </div>)}
+      
+      
+
     </div>
   )
 }
